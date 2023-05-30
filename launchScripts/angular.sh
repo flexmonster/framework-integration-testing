c554a3695545ee33210a90e752f3b4c2b3cbb50b
Solomiia Andrusiv <solja@flexmonster.com>
2023-05-30 14:32:25 +0300

    updated launch scripts

--- launchScripts/angular.sh
+++ launchScripts/angular.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 eval "npm install"
-eval "node framework-integration-testing/launchScripts/writeKeyVariable '../../pivot-angular/node_modules/flexmonster/flexmonster.full.js'"
+eval "node ../framework-integration-testing/launchScripts/writeKeyVariable '../../pivot-angular/node_modules/flexmonster/flexmonster.full.js'"
 eval "ng serve &"
