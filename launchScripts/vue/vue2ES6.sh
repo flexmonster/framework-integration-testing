9f98689a675ae9c0fa59f4a2c8fbebb002765f9d
Solomiia Andrusiv <solja@flexmonster.com>
2023-06-23 18:07:19 +0300

    returned to previous method

--- launchScripts/vue/vue2ES6.sh
+++ launchScripts/vue/vue2ES6.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 eval "cd vue2/ES6"
 eval "npm install"
-eval "node --max_old_space_size=1096 node_modules/.bin/vite build"
 eval "node ../../framework-integration-testing/launchScripts/writeKeyVariable '../../vue2/ES6/node_modules/flexmonster/flexmonster.full.js'"
-eval "node testsServer/server &"
+eval "npm run start &"
