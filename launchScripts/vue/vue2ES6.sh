ab065a8e2020778aeb7266e0ed87293e8040348a
Solomiia Andrusiv <solja@flexmonster.com>
2023-06-23 16:59:03 +0300

    changed server for vue2/es6

--- launchScripts/vue/vue2ES6.sh
+++ launchScripts/vue/vue2ES6.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 eval "cd vue2/ES6"
 eval "npm install"
+eval "vite build"
 eval "node ../../framework-integration-testing/launchScripts/writeKeyVariable '../../vue2/ES6/node_modules/flexmonster/flexmonster.full.js'"
-eval "npm run start &"
+eval "node testsServer/server &"
