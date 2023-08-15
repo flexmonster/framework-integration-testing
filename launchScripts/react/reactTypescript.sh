d45a651bd20fe2e7f972f36a12e8769d13f4b0c7
nadia <66677695+se7enteenblue@users.noreply.github.com>
2023-08-15 12:28:32 +0300

    added ts tests

--- launchScripts/react/reactTypescript.sh
+++ launchScripts/react/reactTypescript.sh
@@ -2,5 +2,5 @@
 eval "cd typescript"
 eval "npm install"
 eval "npm run build"
-eval "node ../framework-integration-testing/launchScripts/writeKeyVariable '../../ES6/node_modules/flexmonster/flexmonster.full.js'"
+eval "node ../framework-integration-testing/launchScripts/writeKeyVariable '../../typescript/node_modules/flexmonster/flexmonster.full.js'"
 eval "node testsServer/server &"
