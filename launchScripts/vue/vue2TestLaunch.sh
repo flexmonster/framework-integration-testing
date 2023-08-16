f6986b90ad4dd1113438ba00c377bc7772cff4e9
Dima Zvazhii <bengorde80@gmail.com>
2023-08-16 15:53:11 +0300

    scripts fix

--- launchScripts/vue/vue2TestLaunch.sh
+++ launchScripts/vue/vue2TestLaunch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 eval "cd ./framework-integration-testing"
 eval "npm install"
+eval "npm install chromedriver"
 eval "npm run test-vue"
--- launchScripts/vue/vue3TestLaunch.sh
+++ launchScripts/vue/vue3TestLaunch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 eval "cd ./framework-integration-testing"
 eval "npm install"
+eval "npm install chromedriver"
 eval "npm run test-vue"
 eval "npm run test-vue-compositionAPI"
