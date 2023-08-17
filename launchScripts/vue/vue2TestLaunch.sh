a83c951a8d650f4f25ad064610dc741345e5a23f
Dima Zvazhii <bengorde80@gmail.com>
2023-08-17 13:39:35 +0300

    latest chromedriver

--- launchScripts/vue/vue2TestLaunch.sh
+++ launchScripts/vue/vue2TestLaunch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 eval "cd ./framework-integration-testing"
 eval "npm install"
-eval "npm install chromedriver"
 eval "npm run test-vue"
--- launchScripts/vue/vue3TestLaunch.sh
+++ launchScripts/vue/vue3TestLaunch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 eval "cd ./framework-integration-testing"
 eval "npm install"
-eval "npm install chromedriver"
 eval "npm run test-vue"
 eval "npm run test-vue-compositionAPI"
